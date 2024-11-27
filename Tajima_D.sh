c1c1399b18425487050184599fd6294304a79d50
Sann Delaive <45197749+Sannouche@users.noreply.github.com>
2024-11-27 13:30:50 -0500

    Update and rename 03_Diversity.sh to Tajima_D.sh

--- Tajima_D.sh
+++ Tajima_D.sh
@@ -2,7 +2,7 @@
 
 # Calculated Nucleotide diversity and Tajima's D by group
 # Run this script from SR_popgen_analysis directory using :
-# srun -c 1 --mem=10G -p small --time=1-00:00:00 -J 03_Pi -o 99_log/03_Pi_%j.log 01_scripts/03_Diversity.sh & 
+# srun -c 1 --mem=10G -p small --time=1-00:00:00 -J 03_Pi -o 99_log/03_Pi_%j.log 01_scripts/Tajima_D.sh & 
 
 #VARIABLES
 VCF="/project/lbernatchez/users/sadel35/SNP_calling_pipeline_202205/06_merged/merged_withoutUn_invariants_V2_2all_FM01_DP4_nonrelated.vcf.gz"
